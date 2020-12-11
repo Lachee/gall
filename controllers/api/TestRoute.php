c31ce36c84c0834a57cd457cc548edc92a0da1ba
Lachee <lachlan.h@hotmail.com>
2020-12-11 19:12:46 +1100

    Added Setup text and removed lightgallery trash

---
+++ controllers/api/TestRoute.php
@@ -0,0 +1,19 @@
+<?php namespace app\controllers\api;
+
+use kiss\Kiss;
+use kiss\router\Route;
+use kiss\router\RouteFactory;
+
+class TestRoute extends Route {
+
+    //We are going to return our routing. Any segment that starts with : is a property.
+    // Note that more explicit routes get higher priority. So /example/apple will take priority over /example/:fish
+    protected static function route() { return "/test"; }
+
+    //HTTP GET on the route. Return an object and it will be sent back as JSON to the client.
+    // Throw an exception to send exceptions back.
+    // Supports get, delete
+    public function get() {
+        return 'test please hold';
+    }
+}
