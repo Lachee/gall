5fac2c5fccdcec6a0d25cc17324279d6a68d7506
Lachee <lachlan.h@hotmail.com>
2021-02-08 20:10:22 +1100

    I have a smooth brain. I cannot really help it. I try my best.

--- kiss/schema/BooleanProperty.php
+++ kiss/schema/BooleanProperty.php
@@ -21,7 +21,7 @@ class BooleanProperty extends Property {
     {
         $val = $value === '' ? 'false' : $value;
         $result = filter_var($val, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
-        if ($result == null){
+        if ($result === null){
             return "Expected a boolean.";
         }
         return parent::validateValue($value);
